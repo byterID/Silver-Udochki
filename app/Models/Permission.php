c571c2409a46c4ab96913a1aa73317a2ee12f1a7
Артемий <lfeanite@gmail.com>
2026-09-02 14:44:04 +0400

    feat(control-panel): добавлены разделы Управление доступом и Группы действий

---
+++ app/Models/Permission.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Permission\Models\Permission as SpatiePermission;
+
+class Permission extends SpatiePermission
+{
+    protected $fillable = ['name', 'title', 'action_group_id', 'guard_name'];
+
+    public function actionGroup(): BelongsTo
+    {
+        return $this->belongsTo(ActionGroup::class);
+    }
+}
